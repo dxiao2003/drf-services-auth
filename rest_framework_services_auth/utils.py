2c7f7063ddfe5cba30563dcde455d7153b3b3352
David Xiao <dxiao@katapal.com>
2016-10-01 00:56:54 -0400

    fix encode username

--- rest_framework_services_auth/utils.py
+++ rest_framework_services_auth/utils.py
@@ -33,7 +33,8 @@ DEFAULT_EXPIRATION_DELAY = 15 * 60  # 15 minutes
 
 def encode_username(service_user_id):
     return base64.b64encode(
-        str(service_user_id).replace('-', '').decode("hex")
+        str(service_user_id).replace('-', '').decode("hex"),
+        altchars="+-"
     ).replace("=", "")
 
 
